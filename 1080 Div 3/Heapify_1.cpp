fd0935940bd1cc046cab7a4334a9f62d69348f95
Sparsh2509 <sparshgupta250904@gmail.com>
2026-02-16 11:48:43 +0530

    Heapify 1 done

---
+++ 1080 Div 3/Heapify_1.cpp
@@ -0,0 +1,42 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int getRoot(int x) {
+    while (x % 2 == 0) {
+        x /= 2;
+    }
+    return x;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n + 1);
+
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+        }
+
+        bool possible = true;
+
+        for (int i = 1; i <= n; i++) {
+            if (getRoot(i) != getRoot(a[i])) {
+                possible = false;
+                break;
+            }
+        }
+
+        cout << (possible ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
